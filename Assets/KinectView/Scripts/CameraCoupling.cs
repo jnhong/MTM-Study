2514ca4479718dd9694d1b1cd580ff8357454fc7
James Hong <pmcspmcspmcs@gmail.com>
2019-03-24 12:22:35 -0500

    first commit

---
+++ Assets/KinectView/Scripts/CameraCoupling.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCoupling : MonoBehaviour
+{
+
+    Camera m_MainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_MainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = m_MainCamera.transform.position;
+    }
+}
---
+++ Assets/MTM-Team/HitBox/HitBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+
+    Renderer rend;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = new Color(0, 1, 0, 0.25f);
+    }
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "HandLeft")
+        {
+            rend.material.color = new Color(1, 0, 0, 0.25f);
+            Debug.Log("OnTriggerEnter\n");
+        }
+
+    }
+
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "HandLeft")
+        {
+            rend.material.color = new Color(0, 1, 0, 0.25f);
+            Debug.Log("OnTriggerExit\n");
+        }
+    }
+    
+}
