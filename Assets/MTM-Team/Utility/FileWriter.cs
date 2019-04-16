82594bd1e1f134855a45eed203b79abce5437e08
ethan sorrell <e.r.sorrell@gmail.com>
2019-04-16 16:39:23 -0500

    added filewriter script and expanded point cloud

--- Assets/MTM-Team/Utility/FileWriter.cs
+++ Assets/MTM-Team/Utility/FileWriter.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FileWriter : MonoBehaviour
 {
+    public string path;
+    public string filename;
+    public Vector2 scrollPosition = Vector2.zero;
+    private string log = "";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!path.EndsWith("\\"))           
+            path = path + "\\";
+        File.WriteAllText(Path.Combine(path, filename), log);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void WriteCode(string code)
     {
-        
+        log += code;
+        File.AppendAllText(Path.Combine(path, filename), code + Environment.NewLine);
+    }
+
+    private void OnGUI()
+    {
+        scrollPosition = GUI.BeginScrollView(new Rect(Screen.width - 110, Screen.height - 110, 100, 100), scrollPosition, new Rect(0, 0, 80, 200));
+        GUI.TextArea(new Rect(0, 0, 80, 200), log);
+        GUI.EndScrollView();
     }
 }
