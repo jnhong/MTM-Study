2e22edd63a13d2cdbb358ebb95b910a8d7893248
ethan sorrell <e.r.sorrell@gmail.com>
2019-04-25 12:54:43 -0500

    added optional functionality to file writer

--- Assets/MTM-Team/Utility/FileWriter.cs
+++ Assets/MTM-Team/Utility/FileWriter.cs
@@ -6,34 +6,41 @@ using UnityEngine;
 
 public class FileWriter : MonoBehaviour
 {
-    /*
     public string path;
     public string filename;
-    public Vector2 scrollPosition = Vector2.zero;
+    public bool writeToFile;
+    public bool showOnGUI;
+
+    private Vector2 scrollPosition = Vector2.zero;
     private string log = "";
 
     // Start is called before the first frame update
     void Start()
     {
-        File.WriteAllText(Path.Combine(path, filename), log);
+        if (writeToFile)
+        {
+            File.WriteAllText(Path.Combine(path, filename), log);
+        }
     }
 
-    public void WriteCode(string code)
+    public void write(string code)
     {
-        log += code;
-        File.AppendAllText(Path.Combine(path, filename), code + Environment.NewLine);
+        log += code + "\n";
+        Debug.Log(code);
+        if (writeToFile)
+        {
+            File.AppendAllText(Path.Combine(path, filename), code + Environment.NewLine);
+        }
     }
 
     private void OnGUI()
     {
-        scrollPosition = GUI.BeginScrollView(new Rect(Screen.width - 110, Screen.height - 110, 100, 100), scrollPosition, new Rect(0, 0, 80, 200));
-        GUI.TextArea(new Rect(0, 0, 80, 200), log);
-        GUI.EndScrollView();
+        if (showOnGUI)
+        {
+            scrollPosition = GUI.BeginScrollView(new Rect(Screen.width - 110, Screen.height - 110, 100, 100), scrollPosition, new Rect(0, 0, 80, 200));
+            GUI.TextArea(new Rect(0, 0, 80, 200), log);
+            GUI.EndScrollView();
+        }
     }
-    */
 
-    public void write(string s)
-    {
-        Debug.Log(s);
-    }
 }
